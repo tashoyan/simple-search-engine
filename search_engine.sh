e24b1fce149af945bcda48a1354e3f8c35394ea1
arseniy.tashoyan <arseniy.tashoyan@hpe.com>
2019-04-02 14:23:06 +0300

    Shell launcher

--- search_engine.sh
+++ search_engine.sh
@@ -4,10 +4,11 @@ set -o nounset
 set -o errexit
 
 work_dir="$(cd "$(dirname -- "$0")" ; pwd)"
+jar_file="$(ls $work_dir/target/*-assembly-*.jar | grep -vi javadoc || true)"
+if test -z "$jar_file"
+then
+    echo "Cannot find the application jar file. Is the project built? Exiting."
+    exit 1
+fi
 
-$SPARK_HOME/bin/spark-submit \
---master 'local[*]' \
---driver-java-options "-Dlog4j.configuration=file://$work_dir/src/test/resources/log4j.xml" \
---driver-memory 2G \
---class com.github.tashoyan.tfidf.Main \
-"$work_dir/target/scala-2.11/spark-sql-tfidf-assembly-0.1-SNAPSHOT.jar" songs_db
+java -jar "$jar_file" songs_db
